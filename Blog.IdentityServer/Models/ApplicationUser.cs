a52b06f6a3c31c77925ceabda573393667d005c1
Zen Master <780589391@qq.com>
2024-03-09 17:33:00 +0800

    导入项目

---
+++ Blog.IdentityServer/Models/ApplicationUser.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+
+namespace Blog.IdentityServer.Models
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser<int>
+    {
+
+        public string LoginName { get; set; }
+
+        public string RealName { get; set; }
+
+        public int sex { get; set; } = 0;
+
+        public int age { get; set; }
+
+        public DateTime birth { get; set; } = DateTime.Now;
+
+        public string addr { get; set; }
+        public string FirstQuestion { get; set; }
+        public string SecondQuestion { get; set; }
+
+        public bool tdIsDelete { get; set; }
+
+        public ICollection<ApplicationUserRole> UserRoles { get; set; }
+    }
+}
